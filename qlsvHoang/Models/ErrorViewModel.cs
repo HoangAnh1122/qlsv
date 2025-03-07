b078beb4a4f63c156eac5518b0bcaa0343211bfc
PC <diepvanhoang2003@gmail.com>
2025-03-07 13:33:17 +0700

    create

---
+++ qlsvHoang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace qlsvHoang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
